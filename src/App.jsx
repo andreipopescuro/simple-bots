e3526643a2c5aef22ff781a740f1690fd5e884ae
Pope <andreipope16@gmail.com>
2023-01-25 22:09:34 +0200

    fixed again routing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/404";
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
