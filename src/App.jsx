e4edcc08bf3e4f9c5a973ad708c219c6431f4531
Pope <andreipope16@gmail.com>
2023-01-25 22:23:01 +0200

    added another page for test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/404";
+import Please from "./pages/Please";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/please" element={<Please />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ src/pages/Please.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Please() {
+  return <div>Please</div>;
+}
+
+export default Please;
