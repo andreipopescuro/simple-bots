5be4c7d2ae25941e33926355b3abb94bb305273d
Pope <andreipope16@gmail.com>
2023-01-25 22:01:15 +0200

    fixed again routing err

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-//routes
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/404";
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
-import { BrowserRouter } from "react-router-dom";
 // openai chat
 // background from mid journey
 // chatbot
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
